e622d9ecff88def320b420f61cef186484d07068
Maxim Pak <zucler@gmail.com>
2018-09-13 10:48:05 +1000

    Updated linux installation script

--- install_vim_ubuntu.sh
+++ install_vim_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp vimrc ~/.vimrc
+cp vimrc_ubuntu ~/.vimrc
 
 vim +PluginInstall +qall 2>&1 > /dev/null
 
@@ -16,3 +16,10 @@ fi
 cd ~/.vim/syntax
 curl http://www.vim.org/scripts/download_script.php\?src_id\=19394 --output nginx.vim
 echo "au BufRead,BufNewFile /etc/nginx/*,/usr/local/nginx/conf/* if &ft == '' | setfiletype nginx | endif" > ~/.vim/filetype.vim
+
+vim -c "spell RU_RU" -c "quit"
+
+if [ ! -d ~/.vim/templates/python ]; then
+mkdir -p ~/.vim/templates/python
+fi
+cat python-google-docstring.txt > ~/.vim/templates/python/block-comment.txt
